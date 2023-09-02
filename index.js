374e6d3298845979b876c9c1432eaf87107be0eb
Laureano Ruben Sotomayor <lrsotomayor466@gmail.com>
2023-09-02 16:23:56 -0300

    agrege la libreria de sweetalert y fetch

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 class Producto {
-  constructor(nombre, tipo, precio) {
+  constructor(nombre, tipo, precio, imagen) {
     this.nombre = nombre;
     this.tipo = tipo;
     this.precio = precio;
+    this.imagen = imagen;
   }
 }
 
-const productos = [
-  new Producto('Pollo', 'carne', 8.0),
-  new Producto('Pescado', 'carne', 12.0),
-  new Producto('Zanahoria', 'verdura', 2.0),
-  new Producto('Morron', 'verdura', 1.5),
-];
-
 let carrito = [];
+let productos = [];
 
 // Obtener el carrito almacenado en localStorage al cargar la página
 if (localStorage.getItem('carrito')) {
@@ -22,42 +17,62 @@ if (localStorage.getItem('carrito')) {
 }
 
 function addToCart(nombre, precio) {
-  const producto = productos.find(producto => producto.nombre === nombre);
+  const cantidadInputId = `${nombre.replace(/\s+/g, '-').toLowerCase()}Cantidad`;
+  const cantidadInput = document.getElementById(cantidadInputId);
 
-  if (producto) {
-    const cantidadInputId = `${nombre.replace(/\s+/g, '-').toLowerCase()}Cantidad`;
-    const cantidadInput = document.getElementById(cantidadInputId);
+  if (cantidadInput) {
+    const cantidad = parseInt(cantidadInput.value);
 
-    if (cantidadInput) {
-      const cantidad = parseInt(cantidadInput.value);
+    if (!isNaN(cantidad) && cantidad > 0) {
+      const productoIndex = carrito.findIndex(item => item.nombre === nombre);
 
-      if (!isNaN(cantidad) && cantidad > 0) {
-        const productoIndex = carrito.findIndex(item => item.nombre === nombre);
-
-        if (productoIndex !== -1) {
-          carrito[productoIndex].cantidad += cantidad;
-        } else {
-          carrito.push({ nombre, precio: producto.precio, cantidad });
+      if (productoIndex !== -1) {
+        carrito[productoIndex].cantidad += cantidad;
+      } else {
+        const producto = productos.find(producto => producto.nombre === nombre);
+        if (producto) {
+          carrito.push({
+            nombre,
+            precio: producto.precio,
+            cantidad
+          });
         }
-
-        updateCartInfo();
-
-        // Guardar el carrito en localStorage
-        localStorage.setItem('carrito', JSON.stringify(carrito));
       }
+
+      // Guardar el carrito en localStorage
+      localStorage.setItem('carrito', JSON.stringify(carrito));
+
+      // Mostrar una alerta personalizada con SweetAlert2
+      Swal.fire({
+        title: 'Producto Agregado',
+        text: `Se ha agregado ${cantidad} ${nombre} al carrito.`,
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      });
+
+      updateCartInfo();
     }
   }
 }
 
 function removeFromCart(nombre) {
   const productoIndex = carrito.findIndex(item => item.nombre === nombre);
-  
+
   if (productoIndex !== -1) {
     carrito.splice(productoIndex, 1);
-    updateCartInfo();
 
     // Guardar el carrito en localStorage
     localStorage.setItem('carrito', JSON.stringify(carrito));
+
+    // Mostrar una alerta de SweetAlert2 cuando se elimine un producto
+    Swal.fire({
+      title: 'Producto Eliminado',
+      text: `Se ha eliminado ${nombre} del carrito.`,
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    });
+
+    updateCartInfo();
   }
 }
 
@@ -83,77 +98,42 @@ function updateCartInfo() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  const container = document.createElement('div');
-  container.className = 'container mt-4';
-  container.innerHTML = `
-    <div id='carritoInfo'></div>
-    <div class="mt-4">
-      <h4>Agregar nuevo producto:</h4>
-      <form id="nuevoProductoForm">
-        <div class="form-group">
-          <label for="nombreNuevoProducto">Nombre:</label>
-          <input type="text" class="form-control" id="nombreNuevoProducto" required>
-        </div>
-        <div class="form-group">
-          <label for="tipoNuevoProducto">Tipo:</label>
-          <input type="text" class="form-control" id="tipoNuevoProducto" required>
-        </div>
-        <div class="form-group">
-          <label for="precioNuevoProducto">Precio:</label>
-          <input type="number" class="form-control" id="precioNuevoProducto" required step="0.01">
-        </div>
-        <button type="button" class="btn btn-success" id="agregarNuevoProductoBtn">Agregar Producto</button>
-      </form>
-    </div>
-  `;
-  document.body.appendChild(container);
+  const productosContainer = document.getElementById('productosContainer');
 
-  productos.forEach(producto => {
-    addProductoForm(producto);
-  });
+  // Reemplaza 'searchTerm' con el término de búsqueda que desees
+  const searchTerm = 'motorola';
 
-  updateCartInfo();
+  fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${encodeURIComponent(searchTerm)}`)
+    .then(response => response.json())
+    .then(data => {
+      productos = data.results.map(producto => new Producto(producto.title, '', producto.price, producto.thumbnail));
 
-  document.getElementById('agregarNuevoProductoBtn').addEventListener('click', function () {
-    const nombreNuevoProducto = document.getElementById('nombreNuevoProducto').value;
-    const tipoNuevoProducto = document.getElementById('tipoNuevoProducto').value;
-    const precioNuevoProducto = parseFloat(document.getElementById('precioNuevoProducto').value);
+      productos.forEach(producto => {
+        const productoCard = document.createElement('div');
+        productoCard.className = 'col-md-4 mb-4';
 
-    if (nombreNuevoProducto && tipoNuevoProducto && !isNaN(precioNuevoProducto) && precioNuevoProducto > 0) {
-      agregarProducto(nombreNuevoProducto, tipoNuevoProducto, precioNuevoProducto);
-      document.getElementById('nuevoProductoForm').reset();
-    } else {
-      alert('Por favor, complete todos los campos correctamente.');
-    }
-  });
+        productoCard.innerHTML = `
+          <div class="card">
+            <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+            <div class="card-body">
+              <h5 class="card-title">${producto.nombre}</h5>
+              <p class="card-text">Precio: $${producto.precio.toFixed(2)}</p>
+              <form class="form-inline">
+                <div class="form-group">
+                  <input type="number" class="form-control" id="${producto.nombre.replace(/\s+/g, '-').toLowerCase()}Cantidad" min="1" value="1">
+                </div>
+                <button type="button" class="btn btn-primary ml-2" onclick='addToCart("${producto.nombre}", ${producto.precio})'>Agregar al Carrito</button>
+              </form>
+            </div>
+          </div>
+        `;
+
+        productosContainer.appendChild(productoCard);
+      });
+
+      updateCartInfo();
+    })
+    .catch(error => {
+      console.error('Error al cargar los productos:', error);
+    });
 });
-
-function addProductoForm(producto) {
-  const productoDiv = document.createElement('div');
-  productoDiv.className = 'list-group-item';
-  productoDiv.innerHTML = `
-    <span>${producto.nombre} - Precio: $${producto.precio.toFixed(2)}</span>
-    <form class="form-inline ml-3">
-      <div class="form-group">
-        <input type="number" class="form-control" id="${producto.nombre.replace(/\s+/g, '-').toLowerCase()}Cantidad" min="1" value="1">
-      </div>
-      <button type="button" class="btn btn-primary ml-2" onclick='addToCart("${producto.nombre}", ${producto.precio})'>Agregar</button>
-    </form>
-  `;
-
-  document.getElementById('carritoInfo').appendChild(productoDiv);
-}
-
-// Agregar nuevos productos al listado
-function agregarProducto(nombre, tipo, precio) {
-  const nuevoProducto = new Producto(nombre, tipo, precio);
-  productos.push(nuevoProducto);
-
-  addProductoForm(nuevoProducto);
-
-  // Guardar el carrito en localStorage
-  carrito.push({ nombre, precio, cantidad: 1 });
-  localStorage.setItem('carrito', JSON.stringify(carrito));
-
-  updateCartInfo();
-}
